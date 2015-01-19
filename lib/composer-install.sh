2f94a20d5432ede0123755e288d0485aa67023de
Anselm Christophersen <ac@anselm.dk>
2015-01-19 15:38:56 +0100

    attempt on making composer install environment aware

--- lib/composer-install.sh
+++ lib/composer-install.sh
@@ -4,6 +4,24 @@
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../.. && pwd )";
 
+
+
 cd $BASEDIR;
 cd public;
-composer install;
+
+
+#this can be called with and witout environment variable
+if [ -z "${1}" ]; then
+	composer install;
+else
+	#getting environment variables
+	ENV="$1";
+	ENVVARS="$BASEDIR/ttools/core/lib/vars-for-env.sh $ENV"
+	eval `$ENVVARS`
+	
+	#echo $ENV_COMPOSERPATH;
+	
+	$ENV_COMPOSERPATH install;
+fi
+
+
